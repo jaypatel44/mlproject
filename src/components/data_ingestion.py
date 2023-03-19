aef8c7e623a606368c4e52e9e89c4f8917fc7a46
Jay <jaybodar1108@gmail.com>
2023-03-19 11:39:19 +0530

    Data Transformation Done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -14,9 +14,9 @@ from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str = os.path.join('artifact',"train.csv")
-    test_data_path: str = os.path.join('artifact',"test.csv")
-    raw_data_path: str = os.path.join('artifact',"raw.csv")
+    train_data_path: str = os.path.join('artifacts',"train.csv")
+    test_data_path: str = os.path.join('artifacts',"test.csv")
+    raw_data_path: str = os.path.join('artifacts',"raw.csv")
 
 class DataIngestion:
     def __init__(self) -> None:
