452ebdf8ac3dd9301880c01b3049e2f428be9608
Jay <jaybodar1108@gmail.com>
2023-03-19 11:31:21 +0530

    Data Transformation Done

--- src/exception.py
+++ src/exception.py
@@ -16,3 +16,10 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
+    
+if __name__=="__main__":        #### Just for testing purpose
+    try:
+        a=4/0
+    except Exception as e:
+        logging.info("Divide by zero error")
+        raise CustomException(e,sys)
